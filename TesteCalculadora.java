4c8b5fe34b14136edfc53085faf620d0bcc27770
Feby Kouyoumdjian Reis <159589782+febyfkr@users.noreply.github.com>
2025-11-26 22:22:00 -0300

    Add files via upload

--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -1,16 +1,41 @@
 package calculadora;
 
+/**		
+ * 		<p><strong>Teste da calculadora</strong></p>
+ * 		<p>Esta classe irá testar a anterior, cada uma das operações:</p>
+ * 		<p>Soma: '+'</p>
+ * 		<p>Subtração: '-'</p>
+ * 		<p>Multiplicação: '*'</p>
+ * 		<p>Divisão: '/'</p>
+ * 		<p>Vale ressaltar o processo de refatoração que estamos aprendendo.</p>
+ * 
+ * @author Frederico K. Reis
+ * @version 1.0
+ */
+
 public class TesteCalculadora {
 
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
+        
+        /**
+         *    Os resultados esperados são respectivamente 5, 6, 15, 4, erro e operador inválido
+         */
+        System.out.println(calc.calcular(2, 3, "+"));  
+        System.out.println(calc.calcular(10, 4, "-")); 
+        System.out.println(calc.calcular(3, 5, "*"));  
+        System.out.println(calc.calcular(8, 2, "/"));  
+        
+        try {
+            System.out.println(calc.calcular(8, 0, "/")); 
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
 
-        // Testes rápidos
-        calc.calc(2, 3, "+");  // esperado: 5
-        calc.calc(10, 4, "-"); // esperado: 6
-        calc.calc(3, 5, "*");  // esperado: 15
-        calc.calc(8, 2, "/");  // esperado: 4
-        calc.calc(8, 0, "/");  // divisao por zero
-        calc.calc(5, 5, "x");  // operação inválida
+        try {
+            System.out.println(calc.calcular(5, 5, "x")); 
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
